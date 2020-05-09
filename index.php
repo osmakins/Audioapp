63600ed886646d69c59b030d8ff7136482d42dca
M.O Akindayo <matthewakinse@gmail.com>
2020-05-09 23:59:28 +0300

    :

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="profile.php">
               <span data-feather="file"></span>
               Profile
             </a>
